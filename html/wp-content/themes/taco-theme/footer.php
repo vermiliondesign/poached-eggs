5ba6c813ac6db9faf7f4b7ee83da50f036242bf5
Brittany <britt.ansay@gmail.com>
2017-05-10 16:31:19 -0600

    fix footer addthis

--- html/wp-content/themes/taco-theme/footer.php
+++ html/wp-content/themes/taco-theme/footer.php
@@ -48,9 +48,7 @@ $theme = AppOption::getInstance();
   else window.addEventListener('load', loadDeferredStyles);
 </script>
 
-
-<!-- Go to www.addthis.com/dashboard to customize your tools -->
-<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-589919bb75172165"></script>
+<!-- addthis script goes here -->
 
 </body>
 </html>
