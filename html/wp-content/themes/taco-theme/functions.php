5b2bafe06f44636fe69dc3db933b1669600f2f68
Ian Zhang <ianzhang@vermilion.com>
2017-06-16 10:59:12 -0600

    Using Taco\Util classes and updating which are included by default

--- html/wp-content/themes/taco-theme/functions.php
+++ html/wp-content/themes/taco-theme/functions.php
@@ -5,15 +5,16 @@ require_once __DIR__.'/app/config.php';
 // load core app files through app.php
 require_once __DIR__.'/app/taco-app.php';
 
-// assign aliases for common util classes
-class Arr extends \AppLibrary\Arr {}
-class Collection extends \AppLibrary\Collection {}
-class Color extends \AppLibrary\Color {}
-class Html extends \AppLibrary\Html {}
-class Num extends \AppLibrary\Num {}
-class Obj extends \AppLibrary\Obj {}
-class Str extends \AppLibrary\Str {}
-class States extends \AppLibrary\States {}
+// assign aliases for common util classes - uncomment as needed
+class Arr extends \Taco\Util\Arr {}
+class Collection extends \Taco\Util\Collection {}
+// class Color extends \Taco\Util\Color {}
+// class Constants extends \Taco\Util\Constants {}
+// class Html extends \Taco\Util\Html {}
+// class Num extends \Taco\Util\Num {}
+// class Obj extends \Taco\Util\Obj {}
+class Str extends \Taco\Util\Str {}
+class View extends \Taco\Util\View {}
 
 require_once __DIR__.'/functions/functions-generic.php';
 require_once __DIR__.'/functions/functions-app.php';
